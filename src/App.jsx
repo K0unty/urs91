aea7a0300e9849045c03be67c052e4fe3aca0d9b
K0unty <kountiapi@pm.me>
2024-07-24 22:44:50 +0530

    getSideBarEntries

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,14 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
@@ -43,7 +45,10 @@ function App() {
 
   return (
     <main className="h-screen my-1 bg-fixed bg-no-repeat bg-center bg-[url('/cy1.jpg')] flex gap-8 ">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
--- src/comp/ProjectsSidebar.jsx
+++ src/comp/ProjectsSidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button.jsx";
 
-export default function ProjectsSidebar({ onStartAddProject }) {
+export default function ProjectsSidebar({ onStartAddProject, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-emerald-950 text-emerald-400 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-strone-200">
@@ -9,7 +9,15 @@ export default function ProjectsSidebar({ onStartAddProject }) {
       <div>
         <Button onClick={onStartAddProject}>+ AddPj</Button>
       </div>
-      <ul></ul>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-cyan-400 hover:text-cyan-200 hover:bg-cyan-800">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
