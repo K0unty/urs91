e9154d62c2fa1ec1c758e9a673ce07700f9db63a
K0unty <kountiapi@pm.me>
2024-07-24 23:11:55 +0530

    styleModal

--- src/comp/Modal.jsx
+++ src/comp/Modal.jsx
@@ -1,5 +1,6 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
+import Button from "./Button";
 
 const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   const dialog = useRef();
@@ -14,11 +15,11 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   return createPortal(
     <dialog
       ref={dialog}
-      className="backdrop:bg-zinc-900/90 p-4 roudned-md shadow-md bg-rose-900 text-rose-500"
+      className="backdrop:bg-zinc-900/90 p-4 rounded-lg shadow-md bg-rose-900 text-rose-500"
     >
       {children}
-      <form method="dialog">
-        <button>{buttonCaption}</button>
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
       </form>
     </dialog>,
     document.getElementById("modal-root")
