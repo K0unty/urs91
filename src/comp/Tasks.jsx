0532d234815036ddc7384c5cbc8f1aa76d5a7135
K0unty <kountiapi@pm.me>
2024-07-25 09:56:52 +0530

    SetupTasks

--- src/comp/Tasks.jsx
+++ src/comp/Tasks.jsx
@@ -1,12 +1,23 @@
 import NewTask from "./NewTask";
 
-export default function Tasks({ onAdd, onDelete }) {
+export default function Tasks({ tasks, onAdd, onDelete }) {
   return (
     <section>
       <h2 className="text-2xl font-box text-green-700 mb-4">Tasks</h2>
       <NewTask onAdd={onAdd} />
-      <p className="text-green-700 my-4">No Tasks</p>
-      <ul></ul>
+      {tasks.length === 0 && <p className="text-rose-700 my-4">No Tasks</p>}
+      {tasks.length > 0 && (
+        <ul className="p-4 mt-8 rounded-md bg-green-100">
+          {tasks.map((task) => (
+            <li key={task.id} className="flex justify-between my-4">
+              <span>{task.text}</span>
+              <button className="transition-all text-green-700 hover:text-rose-500 ">
+                Clear
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
